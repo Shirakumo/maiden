461aff8f566bfca8229173692663a73093093f77
Shinmera <shinmera@tymoon.eu>
2016-02-04 21:59:46 +0100

    Toolkit functions

--- toolkit.lisp
+++ toolkit.lisp
@@ -19,3 +19,31 @@
   `(let (#+sbcl (sb-impl::*default-external-format* ,encoding)
          #+ccl (ccl:*default-external-format* ,encoding))
      ,@body))
+
+(defun package-full-name (package)
+  (loop for max = ""  then (if (< (length max) (length name)) name max)
+        for name in (cons (package-name package) (package-nicknames package))
+        finally (return max)))
+
+(defun starts-with (start string)
+  (and (<= (length start) (length string))
+       (string= start string :end2 (length start))))
+
+(defun left-trim-string (trim string)
+  (if (starts-with trim string)
+      (subseq string (length trim))
+      string))
+
+(defun split (char string)
+  (let ((out (make-string-output-stream))
+        (parts ()))
+    (flet ((push-part ()
+             (let ((part (get-output-stream-string out)))
+               (when (string/= part "")
+                 (push part parts)))))
+      (loop for c across string
+            do (if (char= c char)
+                   (push-part)
+                   (write-char c out))
+            finally (push-part)))
+    (nreverse parts)))
