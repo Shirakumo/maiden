d1210f92c6fcbadc69bc5fb319d4daf1680e0cc7
Shinmera <shinmera@tymoon.eu>
2016-02-04 23:11:08 +0100

    Started work on serializer

---
+++ modules/serialize/package.lisp
@@ -0,0 +1,10 @@
+#|
+ This file is a part of Colleen
+ (c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:colleen-user)
+(defpackage #:colleen-serialize
+  (:nicknames #:irc #:org.shirakumo.colleen.modules.serialize)
+  (:use))
