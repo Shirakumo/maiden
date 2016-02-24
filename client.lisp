7d49d0b12615e6d539c4ed378223b022e8c314f8
Shinmera <shinmera@tymoon.eu>
2016-02-24 13:00:46 +0100

    Updated client stuff to work with new system (mostly removed all the baggage around keeping track of it all.

--- client.lisp
+++ client.lisp
@@ -6,76 +6,16 @@
 
 (in-package #:org.shirakumo.colleen)
 
-(defvar *clients* ())
-(defvar *clients-lock* (bt:make-lock "Colleen clients registry lock"))
+(define-consumer client () ())
 
-(deeds:define-cached-slots-class client ()
-  ((name :initarg :name :reader name))
-  (:default-initargs
-   :name (error "NAME required.")))
-
-(defmethod shared-initialize :around ((client client) slot-names &rest args &key name)
-  (etypecase name
-    (string (apply #'call-next-method client slot-names :name (kw name) args))
-    (symbol (call-next-method))
-    (null (call-next-method))))
-
-(defmethod initialize-instance :after ((client client) &key)
-  (add-client client))
-
-(defmethod print-object ((client client) stream)
-  (print-unreadable-object (client stream :type T)
-    (format stream "~a" (name client))))
-
-(defgeneric client (client-ish))
-(defgeneric add-client (client))
-(defgeneric remove-client (client))
-
-(defmethod client ((name string))
-  (client (kw name)))
-
-(defmethod client ((name symbol))
-  (find name *clients* :test #'string-equal :key #'name))
-
-(defmethod client ((client client))
-  client)
-
-(defun list-clients ()
-  (bt:with-lock-held (*clients-lock*)
-    (copy-list *clients*)))
-
-(defmethod add-client ((client client))
-  (bt:with-lock-held (*clients-lock*)
-    (let ((existing (loop for cons on *clients*
-                          when (string-equal (name (car cons)) (name client))
-                          return cons)))
-      (cond (existing
-             (cerror "Replace the client." 'client-already-exists-error
-                     :client client :existing-client (car existing))
-             (setf (car existing) client))
-            (T
-             (push client *clients*))))))
-
-(defmethod remove-client ((client client))
-  (bt:with-lock-held (*clients-lock*)
-    (setf *clients* (remove client *clients*))))
-
-(defmacro define-client (name direct-superclasses direct-slots &rest options)
-  (when (loop for super in direct-superclasses
-              never (c2mop:subclassp (find-class super) (find-class 'client)))
-    (push 'client direct-superclasses))
-  `(deeds:define-cached-slots-class ,name ,direct-superclasses
-     ,direct-slots
-     ,@options))
-
-(define-client user-client ()
+(define-consumer user-client (client)
   ())
 
 (defgeneric authenticate (sender client)
   (:method (sender (client client))
     NIL))
 
-(define-client remote-client ()
+(define-consumer remote-client (client)
   ())
 
 (defmethod print-object ((client remote-client) stream)
@@ -98,7 +38,7 @@
   (when (client-connected-p client)
     (cerror "Remove the client anyway." 'client-still-connected-error :client client)))
 
-(define-client server-client (remote-client)
+(define-consumer server-client (remote-client)
   ((host :initarg :host :accessor host)
    (port :initarg :port :accessor port)
    (encoding :initarg :encoding :accessor encoding))
