ea9e31bc3fe56e64541eac108fc2736c8e3abab5
Shinmera <shinmera@tymoon.eu>
2016-02-24 10:00:32 +0100

    Made component system work (at least as far as I can tell)

--- component.lisp
+++ component.lisp
@@ -21,25 +21,58 @@
 (defmethod c2mop:validate-superclass ((class component-class) (superclass component-class))
   T)
 
-(defclass component ()
-  ((handlers :initform () :accessor handlers))
+(defmethod (setf handlers) :after (handlers (class component-class))
+  (make-instances-obsolete class))
+
+(defclass component (named-entity)
+  ((handlers :initform () :accessor handlers)
+   (cores :initform () :accessor cores))
   (:metaclass component-class))
 
 (defmethod initialize-instance :after ((component component) &key)
   (dolist (handler (handlers (class-of component)))
     (push (instantiate-handler handler component) (handlers component))))
 
+(defmethod update-instance-for-redefined-class :after ((component component) added-slots discarded-slots plist &rest initargs)
+  (declare (ignore added-slots discarded-slots plist initargs))
+  (v:info :colleen.core.component "~a updating handlers." component)
+  (let ((cores (cores component)))
+    ;; Deregister
+    (remove-consumer component cores)
+    ;; Rebuild
+    (stop component)
+    (setf (handlers component) ())
+    (dolist (handler (handlers (class-of component)))
+      (push (instantiate-handler handler component) (handlers component)))
+    ;; Reregister
+    (add-consumer component cores)))
+
 (defmethod add-consumer :after ((component component) (core core))
   (dolist (handler (handlers component))
-    (register-handler handler core)))
+    (register-handler handler core))
+  (push core (cores component)))
+
+(defmethod remove-consumer ((component component) (everywhere (eql T)))
+  (dolist (core (cores component))
+    (remove-consumer component core)))
 
 (defmethod remove-consumer :after ((component component) (core core))
   (dolist (handler (handlers component))
-    (deregister-handler handler core)))
+    (deregister-handler handler core))
+  (setf (cores component) (remove core (cores component))))
+
+(defmethod start ((component component))
+  (dolist (handler (handlers component))
+    (start handler)))
+
+(defmethod stop ((component component))
+  (dolist (handler (handlers component))
+    (stop handler)))
 
 (defclass abstract-handler ()
   ((target-class :initarg :target-class :accessor target-class)
-   (options :initarg :options :accessor options))
+   (options :initarg :options :accessor options)
+   (name :initarg :name :accessor name))
   (:default-initargs
    :target-class 'queued-handler
    :options ()))
@@ -47,38 +80,46 @@
 (defmethod initialize-instance :after ((handler abstract-handler) &rest args &key &allow-other-keys)
   (let ((class (getf args :target-class)))
     (when class (setf (target-class handler) class))
-    (setf (options handler) (deeds::removef args :target-class))))
+    (setf (options handler) (deeds::removef args :target-class :options))))
 
-;; What to do if we need specialising on the particular component?
 (defmethod instantiate-handler ((handler abstract-handler) (component component))
-  (apply #'make-instance
-         (target-class handler)
-         :delivery-function (lambda (event)
-                              (funcall (getf (options handler) :delivery-function) component event))
-         (deeds::removef (options handler) :delivery-function)))
-
-(defun make-abstract-handler (target-class &rest options)
-  (apply #'make-instance 'abstract-handler :target-class target-class :options options))
+  (let* ((options (options handler))
+         (filter (getf options :filter))
+         (delivery (getf options :delivery-function))
+         (match (getf options :match-component)))
+    ;; Extend filter to match component.
+    (when match
+      (if (eql match T)
+          (setf filter `(and (eq ,component component) ,filter))
+          (setf filter `(and (eq ,component ,match) ,filter))))
+    (apply #'make-instance
+           (target-class handler)
+           :delivery-function (lambda (event) (funcall delivery component event))
+           :filter filter
+           (deeds::removef options :delivery-function :filter :match-component))))
 
 (defmacro define-handler ((component name event-type) args &body body)
   (destructuring-bind (compvar event &rest args) args
     (multiple-value-bind (options body) (deeds::parse-into-kargs-and-body body)
       (let ((class (or (getf options :class) 'queued-handler))
             (options (deeds::removef options :class)))
-        `(pushnew (make-abstract-handler
-                   ',class
-                   :name ',name
-                   :event-type ',event-type
-                   :delivery-function (lambda (,compvar ,event)
-                                        (with-origin (',name)
-                                          (with-fuzzy-slot-bindings ,args (,event ,event-type)
-                                            ,@body)))
-                   ,@options)
-                  (handlers (find-class ',component) :test #'name))))))
+        `(update-list (make-instance
+                       'abstract-handler
+                       :target-class ',class
+                       :name ',name
+                       :event-type ',event-type
+                       :delivery-function (lambda (,compvar ,event)
+                                            (declare (ignorable ,compvar ,event))
+                                            (with-origin (',name)
+                                              (with-fuzzy-slot-bindings ,args (,event ,event-type)
+                                                ,@body)))
+                       ,@options)
+                      (handlers (find-class ',component))
+                      :key #'name)))))
 
 (defmacro define-component (name direct-superclasses direct-slots &rest options)
-  (unless (loop for super in direct-superclasses
-                never (c2mop:subclassp (find-class super) (find-class 'component)))
+  (when (loop for super in direct-superclasses
+              never (c2mop:subclassp (find-class super) (find-class 'component)))
     (push 'component direct-superclasses))
   (unless (find :metaclass options :key #'first)
     (push `(:metaclass component-class) options))
