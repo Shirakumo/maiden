351e4e1dab9e7da4d8021284305977827838ba24
Shinmera <shinmera@tymoon.eu>
2016-03-08 08:35:23 +0100

    Added exports

--- clients/relay/package.lisp
+++ clients/relay/package.lisp
@@ -8,5 +8,52 @@
 (define-module #:colleen-relay
   (:nicknames #:org.shirakumo.colleen.clients.relay)
   (:use #:cl #:colleen #:colleen-serialize)
-  ;; 
-  (:export))
+  ;; client.lisp
+  (:export
+   #:connection-initiated
+   #:connection-closed
+   #:relay-client
+   #:remote
+   #:relay-client-initiator)
+  ;; conditions.lisp
+  (:export
+   #:relay-condition   
+   #:carrier-condition
+   #:message
+   #:target-condition
+   #:target
+   #:no-relay-target-specified
+   #:relay-route-not-found
+   #:relay-link-not-found
+   #:client-version-mismatch)
+  ;; containers.lisp
+  (:export
+   #:subscription-update
+   #:target
+   #:subscriber
+   #:subscription
+   #:event-type
+   #:filter
+   #:unsubscription
+   #:transport
+   #:event
+   #:target
+   #:make-transport
+   #:network-update
+   #:new
+   #:bad
+   #:make-network-update
+   #:virtual-client
+   #:links
+   #:make-virtual-clients)
+  ;; relay.lisp
+  (:export
+   #:relay
+   #:subscriptions
+   #:my-subscriptions
+   #:routable-p
+   #:update
+   #:relay
+   #:connect
+   #:subscribe
+   #:unsubscribe))
