1b10abf27fa70ffdd738dc1609b0235e1f870cf8
Shinmera <shinmera@tymoon.eu>
2016-05-21 20:35:02 +0200

    Add time agent

---
+++ agents/time/package.lisp
@@ -0,0 +1,18 @@
+#|
+ This file is a part of Maiden
+ (c) 2016 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:maiden-user)
+(defpackage #:maiden-time
+  (:nicknames #:org.shirakumo.maiden.agents.time)
+  (:use #:cl #:maiden #:maiden-api-access #:maiden-commands)
+  ;; time.lisp
+  (:shadow #:time)
+  (:export
+   #:timezone-data
+   #:timezone
+   #:local-time
+   #:user-time
+   #:time))
---
+++ agents/time/time.lisp
@@ -0,0 +1,65 @@
+#|
+ This file is a part of Maiden
+ (c) 2016 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.maiden.agents.time)
+
+(defparameter *timezone-api* "https://maps.googleapis.com/maps/api/timezone/json")
+
+(maiden-accounts:define-fields
+  (timezone () "The time zone the account's user is currently in."))
+
+(defun timezone-data (location &optional (time (get-unix-time)))
+  (let* ((location (if (listp location) location (maiden-location:coordinates location)))
+         (data (request *timezone-api* :get `(("sensor" "false")
+                                              ("timestamp" ,time)
+                                              ("location" ,(format NIL "~f,~f" (first location) (second location))))))
+         (status (json-v data "status")))
+    (cond ((string-equal status "ok")
+           (list :zone-id (json-v data "timeZoneId")
+                 :zone (json-v data "timeZoneName")
+                 :dst-offset (json-v data "dstOffset")
+                 :offset (json-v data "rawOffset")))
+          ((string-equal status "zero_results")
+           (values))
+          ((string-equal status "over_query_limit")
+           (error "Exceeded allowed amount of queries against the Google Maps API."))
+          (T
+           (error "Google Maps failed to perform your request for an unknown reason.")))))
+
+(defun timezone (location)
+  (let ((data (timezone-data location)))
+    (values (getf data :zone) (getf data :zone-id))))
+
+(defun time (location)
+  (let ((data (timezone-data location)))
+    (+ (get-universal-time)
+       (getf data :offset)
+       (getf data :dst-offset))))
+
+(defun user-time (user)
+  (let ((account (account user :error NIL)))
+    (or (when account
+          (local-time (or (field 'timezone account NIL)
+                          (user-location user))))
+        (error "I don't know where ~a is located." user))))
+
+(define-consumer time ()
+  ())
+
+(define-command (time timezone-location) (c ev location)
+  :command "timezone of"
+  (let* ((data (timezone-data location))
+         (secs (+ (getf data :offset) (getf data :dst-offset))))
+    (reply ev "The time zone for ~s is ~a (UTC~@f)"
+           (getf data :zone) (/ secs 60))))
+
+(define-command (time time-location) (c ev location)
+  :command "time in"
+  (reply ev "The time in ~a is ~a." location (format-absolute-time (time location))))
+
+(define-command (time time-user) (c ev user)
+  :command "time for"
+  (reply ev "The time for ~a is ~a." user (format-absolute-time (user-time user))))
