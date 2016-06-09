b75fdff623a86f6229c2c97ed717e506c86b8c2e
Shinmera <shinmera@tymoon.eu>
2016-06-09 08:40:09 +0200

    Fixed interface to more likely be to work.

--- agents/markov/interface.lisp
+++ agents/markov/interface.lisp
@@ -7,16 +7,26 @@
 (in-package #:org.shirakumo.maiden.agents.markov)
 
 (define-consumer markov ()
-  ((storage)))
+  ((storage :initform NIL)
+   (save-counter :initform 0 :accessor save-counter)
+   (save-frequency :initform 20 :accessor save-frequency)
+   (file :initform (asdf:system-relative-pathname :maiden-markov "markov.dat") :accessor file)))
 
 (defmethod storage ((markov markov))
   (or (slot-value markov 'generator)
       (setf (slot-value markov 'generator)
-            (read-generator (asdf:system-relative-pathname :maiden-markov "markov.dat")))))
+            (read-generator (file markov)))))
+
+(defun maybe-save (markov)
+  (incf (save-counter markov))
+  (when (<= (save-frequency markov) (save-counter markov))
+    (setf (save-counter markov) 0)
+    (write-generator (storage markov) (file markov))))
 
 (define-handler (markov handle message-event) (c ev message)
   :class activatable-handler
-  (learn message (generator c)))
+  (learn message (generator c))
+  (maybe-save c))
 
 (define-command (markov ramble) (c ev)
   (reply ev "~a" (make-sentence (generator c))))
