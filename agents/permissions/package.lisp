52bfd6830e4e28f00844a6b4d98dc48982fc4553
Shinmera <shinmera@tymoon.eu>
2016-06-12 13:15:48 +0200

    Exports

--- agents/permissions/package.lisp
+++ agents/permissions/package.lisp
@@ -8,4 +8,20 @@
 (defpackage #:maiden-permissions
   (:nicknames #:org.shirakumo.maiden.agents.permissions)
   (:use #:cl #:maiden #:maiden-commands #:maiden-storage)
-  )
+  ;; permissions.lisp
+  (:export
+   #:permission-denied
+   #:user
+   #:perm
+   #:normalize-permission
+   #:perm-match-p
+   #:user-perm
+   #:administrator-p
+   #:add-administrator
+   #:remove-administrator
+   #:add-default-permission
+   #:remove-default-permission
+   #:allowed-p
+   #:check-allowed
+   #:with-permission
+   #:permissions))
