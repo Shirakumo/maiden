2df18ff59c3e27a8031ae1a23b73d285f179dd5d
Shinmera <shinmera@tymoon.eu>
2016-06-24 16:34:07 +0200

    Implement send-to for framework-user

--- agents/commands/invocation.lisp
+++ agents/commands/invocation.lisp
@@ -20,6 +20,9 @@
    :user *framework-user*
    :message ""))
 
+(defmethod send-to ((user (eql *framework-user*)) format-string &rest format-args)
+  (v:info :commands "=> ~?" (list* format-string format-args)))
+
 (defmethod reply ((event framework-message) format-string &rest format-args)
   (apply #'v:info :commands format-string format-args))
 
