8722c2844fc743abb000fdd7457e87878686404c
Shinmera <shinmera@tymoon.eu>
2017-02-16 20:30:15 +0100

    Docs for maiden-serialize

--- modules/serialize/documentation.lisp
+++ modules/serialize/documentation.lisp
@@ -6,4 +6,38 @@
 
 (in-package #:org.shirakumo.maiden.modules.serialize)
 
+;; serialize.lisp
+(docs:define-docs
+  (variable *finders*
+    "Association list tying a \"type\" to a function that can resolve an instance of that \"type\" from an ID.
 
+See FIND-INSTANCE")
+
+  (variable *event-code*
+    "The CL-STORE code assigned to events.")
+
+  (variable *core-code*
+    "The CL-STORE code assigned to cores.")
+
+  (variable *consumer-code*
+    "The CL-STORE code assigned to consumers.")
+
+  (variable *footer-buffer*
+    "The buffer to store the gzip footer in.")
+
+  (function find-instance
+    "Attempts to resolve the ID of the given \"type\" to an instance using the finders.
+
+See *FINDERS*")
+
+  (function serialize
+    "Serialize the given object to the given target stream.
+
+The data is serialized, GZIP compressed, and then put to the stream.")
+
+  (function deserialize
+    "Deserialize an object from the given stream.
+
+FINDERS must be a suitable alist that can find COREs and CONSUMERs by their ID.
+
+See FIND-INSTANCE"))
