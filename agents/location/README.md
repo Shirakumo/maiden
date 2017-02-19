cfd061255a4bdb7a951bf86240e7d73c53aa6958
Shinmera <shinmera@tymoon.eu>
2017-02-19 14:41:42 +0100

    Docs for location

---
+++ agents/location/README.md
@@ -0,0 +1,2 @@
+## About
+This provides an agent and some functions to perform geolocation. It allows retrieving the address of an ambiguous location, as well as getting the exact geological latitude and longitude coordinates of a place. This is done through the Google Maps geocoding API.
--- agents/location/documentation.lisp
+++ agents/location/documentation.lisp
@@ -7,6 +7,33 @@
 (in-package #:org.shirakumo.maiden.agents.location)
 
 (docs:define-docs
+  (variable *geocode-api*
+    "Address of the google maps geocode API.")
+
+  (function geo-information
+    "Retrieve geolocation information about the given location.
+
+Errors are signalled in the following cases:
+- No results could be found for the location
+- You exceeded the maximum number of queries allowed
+- Some other failure on behalf of the Google API.
+
+See *GEOCODE-API*")
+
+  (function coordinates
+    "Return the coordinates of a location.
+
+Returns two values:
+- A list of the latitude and longitude of the location
+- The long name of the resolved address of the location
+
+See GEO-INFORMATION")
+
+  (function address
+    "Attempt to find the closes address for the location.
+
+See GEO-INFORMATION")
+  
   (type location
     "This agent helps with location information such as geocoding using the Google Maps API.")
 
