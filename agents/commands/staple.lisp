b61902e4be0b04707b58aa3e122fd8f253ed9393
Shinmera <shinmera@tymoon.eu>
2017-02-19 14:37:44 +0100

    Fix docs

--- agents/commands/staple.lisp
+++ agents/commands/staple.lisp
@@ -3,16 +3,20 @@
 (defclass symb-command (staple:symb-function)
   ())
 
+(defmethod staple:symb-name ((symb symb-command))
+  (maiden-commands:prefix
+   (maiden-commands:command-invoker (slot-value symb 'symbol))))
+
 (defmethod staple:symb-function ((symb symb-command))
   (maiden-commands::invoker
-   (maiden-commands:command-invoker (staple:symb-symbol symb))))
+   (maiden-commands:command-invoker (slot-value symb 'symbol))))
 
 (defmethod staple:symb-documentation ((symb symb-command))
-  (documentation (staple:symb-symbol symb) 'maiden-commands:command))
+  (documentation (slot-value symb 'symbol) 'maiden-commands:command))
 
 (defmethod staple:symb-arguments ((symb symb-command))
   (maiden-commands::lambda-list
-   (maiden-commands:command-invoker (staple:symb-symbol symb))))
+   (maiden-commands:command-invoker (slot-value symb 'symbol))))
 
 (defmethod staple:symb-type-order ((symb (eql 'symb-command)))
   (1+ (staple:symb-type-order 'symb-function)))
