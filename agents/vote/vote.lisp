a8e5f0bb348135a37da86c06ce3ad72a8ed21aa0
Shinmera <shinmera@tymoon.eu>
2017-03-09 11:47:36 +0100

    Automatically end vote if all users in channel have voted (untested)

--- agents/vote/vote.lisp
+++ agents/vote/vote.lisp
@@ -15,6 +15,18 @@
 (defun letter->integer (char)
   (- (char-code (char-downcase char)) (1- (char-code #\a))))
 
+(defun end-vote (c ev)
+  (let ((options (gethash (channel ev) (votes c))))
+    (when options
+      (remhash (channel ev) (votes c))
+      (let* ((options (sort (rest options) #'> :key #'length))
+             (winning (first options)))
+        (reply ev "Voting is now closed. The winning option, with ~d out of ~d votes, is:"
+               (length (rest winning))
+               (loop for option in options
+                     summing (length (rest option))))
+        (reply ev "  ~a" (first winning))))))
+
 (define-command (vote start-vote) (c ev &rest options)
   :command "vote between"
   (when (gethash (channel ev) (votes c))
@@ -33,10 +45,16 @@
 
 (define-handler (vote vote-counter (and message-event passive-event)) (c ev message)
   (cl-ppcre:register-groups-bind (option) ("\\s*(\\w)[.)!]\\s*" message)
-    (let ((option (nth (1- (letter->integer (elt option 0)))
-                       (rest (gethash (channel ev) (votes c))))))
+    (let ((options (rest (gethash (channel ev) (votes c))))
+          (option (nth (1- (letter->integer (elt option 0))) options)))
       (when (and option (not (find (user ev) (cdr option))))
-        (setf (cdr option) (cons (user ev) (cdr option)))))))
+        (setf (cdr option) (cons (user ev) (cdr option)))
+        ;; If all users in the channel voted, end it automatically.
+        (when (loop for user in (users (channel ev))
+                    always (or (eql (name user) (username c))
+                               (loop for option in options
+                                     thereis (find user (rest option)))))
+          (end-vote c ev))))))
 
 (define-command (vote end-vote) (c ev option)
   :command "end vote"
@@ -45,11 +63,4 @@
       (error "There is no vote going on in this channel."))
     (unless (eql (first options) (user ev))
       (error "Only the user who started the vote may end it."))
-    (remhash (channel ev) (votes c))
-    (let* ((options (sort (rest options) #'> :key #'length))
-           (winning (first options)))
-      (reply ev "Voting is now closed. The winning option, with ~d out of ~d votes, is:"
-             (length (rest winning))
-             (loop for option in options
-                   summing (length (rest option))))
-      (reply ev "  ~a" (first winning)))))
+    (end-vote c ev)))
