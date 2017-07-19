3e071758a3399f07d9b8083b705da3314e89b991
Shinmera <shinmera@tymoon.eu>
2017-07-19 13:39:57 +0200

    Add shorthands to lookup

--- agents/lookup/interface.lisp
+++ agents/lookup/interface.lisp
@@ -13,3 +13,12 @@
   :command "look up"
   (destructuring-bind (url &optional title) (look-up archive term)
     (reply ev "~@[~@(~a~) ~]~a" title url)))
+
+(defmacro define-shorthand-command (name &key (archive (string name)) (command (string name)))
+  `(define-command (lookup ,name) (c ev &string term)
+     :command ,command
+     (issue (make-instance 'look-up :term term :archive ,(string archive) :dispatch-event ev)
+            (core ev))))
+
+(define-shorthand-command clhs)
+(define-shorthand-command mop)
